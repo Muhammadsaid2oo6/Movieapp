33c790fa37266a488f27a1ef1306a48fb7b82eef
Muhammadsaid2oo6 <muhammadsaid06hasanboyev@gmail.com>
2022-08-06 20:14:55 +0500

    Bitay deb qoldi

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Movie from "./Movie";
 
-export class Main extends React.Component {
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=";
 
-    render() {
-        return(
-            <div className="container content">
-                all content
-            </div>
-        )
-    }
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+
+export function Main() {
+  
+    const [ movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch(FEATURED_API)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+            setMovies(data.results)
+        }) 
+    }, [])
+
+    return (
+      <div className="movie-container content">
+        {movies.length > 0 &&  movies.map((movie) => (
+          <Movie key={movie.id}  {...movie}/>
+        ))}
+      </div>
+    );
+  
 }
